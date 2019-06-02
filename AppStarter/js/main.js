068681cd4169b7cf8a9ec087974e96de0256f4eb
Yura747 <48203127+Yura747@users.noreply.github.com>
2019-06-02 21:33:42 +0300

    page deleted

--- AppStarter/js/main.js
+++
@@ -1,7 +0,0 @@
-
-
-$('.hamburger').on('click', function(){
-    $(this).toggleClass('active');
-    $('.mobile-menu-wrapper').toggleClass('active');
-    $('body').toggleClass('menu-active ');
-});
