50d94ea11bda76e77c9d96c500c9cfb67bd8591d
Yura747 <48203127+Yura747@users.noreply.github.com>
2019-06-09 22:07:51 +0300

    href anchors added

--- AppStarter/js/main.js
+++ AppStarter/js/main.js
@@ -5,3 +5,34 @@ $('.hamburger').on('click', function(){
     $('.mobile-menu-wrapper').toggleClass('active');
     $('body').toggleClass('menu-active ');
 });
+
+
+$(document).ready(function() {
+    var margin = 100; 
+    $("a").click(function() { 
+       $('.hamburger').removeClass('active');
+       $('.mobile-menu-wrapper').removeClass('active');
+       $('body').removeClass('menu-active');
+       window.location.hash = $(this).attr("href");
+
+       $("html, body").animate({
+          scrollTop: $($(this).attr("href")).offset().top - margin + "px" 
+       }, {
+          duration: 300, 
+          easing: "swing"
+       });
+       return false;
+    });
+ });
+
+
+ $('#get-free').on('click', function() {
+    var margin = 100; 
+    $("html, body").animate({
+       scrollTop: $('#get-it-free').offset().top - margin + "px" 
+    }, {
+       duration: 300, 
+       easing: "swing"
+    });
+    return false;
+});
