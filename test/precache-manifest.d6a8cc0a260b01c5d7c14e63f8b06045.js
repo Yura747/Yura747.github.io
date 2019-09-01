726f37123a7d6c613be8a2f80de1463a5dc0ed5e
Yura747 <48203127+Yura747@users.noreply.github.com>
2019-09-01 23:25:31 +0300

    test

---
+++ test/precache-manifest.d6a8cc0a260b01c5d7c14e63f8b06045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f337e638a4954301c909e2b3d8b1b52f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2252ea8d0f2d338f3b15",
+    "url": "/static/css/main.39d92f8f.chunk.css"
+  },
+  {
+    "revision": "5fbd81ed17027738187b",
+    "url": "/static/js/2.836fcbee.chunk.js"
+  },
+  {
+    "revision": "2252ea8d0f2d338f3b15",
+    "url": "/static/js/main.ce8c7073.chunk.js"
+  },
+  {
+    "revision": "b96dd0be2fce88e8af8d",
+    "url": "/static/js/runtime~main.829b2fc0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  }
+]);
