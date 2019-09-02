e56c5e2818291c6e2a4c2f5f4984fd7fb1e97098
Yura747 <48203127+Yura747@users.noreply.github.com>
2019-09-02 23:04:10 +0300

    Git Api production version

---
+++ GitProd/precache-manifest.04ea4edeec7cbec0e0c4a24a14b0b01f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434b49fd1f491af9c7996eb5f2f94d88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a52777448b9e7c11136",
+    "url": "/static/css/main.14957dec.chunk.css"
+  },
+  {
+    "revision": "6107d1ba5f47f6f69f66",
+    "url": "/static/js/2.b890b88f.chunk.js"
+  },
+  {
+    "revision": "4a52777448b9e7c11136",
+    "url": "/static/js/main.f6ba00cd.chunk.js"
+  },
+  {
+    "revision": "b96dd0be2fce88e8af8d",
+    "url": "/static/js/runtime~main.829b2fc0.js"
+  }
+]);
