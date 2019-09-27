ceaa091e3a63a8e26a5ba1854668d774b2c3ba12
Yura747 <48203127+Yura747@users.noreply.github.com>
2019-09-27 20:27:57 +0300

    dfdffg

---
+++ build/precache-manifest.d23bbcb2c5639ec7fb6af08ea78a55da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad904fd23ac625ea3704953ca2277102",
+    "url": "yura747.github.io/index.html"
+  },
+  {
+    "revision": "37a89f8931caed268caf",
+    "url": "yura747.github.io/static/css/2.053a83bb.chunk.css"
+  },
+  {
+    "revision": "0dbd0973f893de6612bb",
+    "url": "yura747.github.io/static/css/main.b9d09fc1.chunk.css"
+  },
+  {
+    "revision": "37a89f8931caed268caf",
+    "url": "yura747.github.io/static/js/2.dda6e453.chunk.js"
+  },
+  {
+    "revision": "0dbd0973f893de6612bb",
+    "url": "yura747.github.io/static/js/main.962eaea5.chunk.js"
+  },
+  {
+    "revision": "640b13ea4337ab14ebb7",
+    "url": "yura747.github.io/static/js/runtime~main.7219d33f.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "yura747.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "yura747.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "yura747.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "yura747.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
