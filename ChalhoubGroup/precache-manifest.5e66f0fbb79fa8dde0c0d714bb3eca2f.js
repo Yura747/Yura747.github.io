6c73a16c4c6b044c190cc5f52d8a0cd92ae20618
Yurii <yura747@ukr.net>
2020-06-21 20:48:09 +0300

    ChalhoubGroup commit

---
+++ ChalhoubGroup/precache-manifest.5e66f0fbb79fa8dde0c0d714bb3eca2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4218d8aa3ce64425577706885d1b300",
+    "url": "/ChalhoubGroup/index.html/index.html"
+  },
+  {
+    "revision": "2493978dde0c944cefce",
+    "url": "/ChalhoubGroup/index.html/static/css/main.2bdf9bab.chunk.css"
+  },
+  {
+    "revision": "185b8b406d049a926ac7",
+    "url": "/ChalhoubGroup/index.html/static/js/2.2e6b9cfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ChalhoubGroup/index.html/static/js/2.2e6b9cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2493978dde0c944cefce",
+    "url": "/ChalhoubGroup/index.html/static/js/main.8f423aa3.chunk.js"
+  },
+  {
+    "revision": "e500efd4ab2ec466d221",
+    "url": "/ChalhoubGroup/index.html/static/js/runtime-main.818f86e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ChalhoubGroup/index.html/static/media/logo.5d5d9eef.svg"
+  }
+]);
