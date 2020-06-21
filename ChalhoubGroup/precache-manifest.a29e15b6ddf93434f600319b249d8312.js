8ad02ab9d3c5bf8f01bf5149ec6857bfb0be1a64
Yurii <yura747@ukr.net>
2020-06-21 21:45:00 +0300

    Build 2

---
+++ ChalhoubGroup/precache-manifest.a29e15b6ddf93434f600319b249d8312.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536c9f9ca79ec074259cecc58e56ccd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751bd857d8bbf66de9d5",
+    "url": "/static/css/main.2bdf9bab.chunk.css"
+  },
+  {
+    "revision": "0f5ac5f69cc8a0d0c1be",
+    "url": "/static/js/2.8c709aad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c709aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751bd857d8bbf66de9d5",
+    "url": "/static/js/main.46ca44bc.chunk.js"
+  },
+  {
+    "revision": "9b0913968f0601c8ee00",
+    "url": "/static/js/runtime-main.408499cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
