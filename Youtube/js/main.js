dd5bcb0d632c275b4b3940f2a556400ac5655eb5
Yuriy Nibit <yura747@ukr.net>
2020-11-08 23:48:02 +0200

    Youtube updates
    - API key updated
    - code logic changed
    - html/styles updates
    - form added

--- Youtube/js/main.js
+++ Youtube/js/main.js
@@ -1,81 +1,106 @@
-
-const list = document.querySelector('.list');
-const video = document.querySelector('.video');
-const open = document.querySelector('.open');
-const videoInner = document.querySelector('.video__inner');
+const $form = document.querySelector(".form");
 
 const YOUTUBE = {
-  init() {
-    this.load();
-  },
+
   params: {
-    url: 'https://www.googleapis.com/youtube/v3/search?',
-    KEY: 'AIzaSyAeedIvfAxkWBPSiFMrQDQlcAZAf5BW1Gw',
-    part: 'snippet',
-    type: 'video',
-    maxResults: '15',
-    order: 'date',
-    searchFor: 'JavaScript|python -basic',
-    videoCaption: 'closedCaption',
-    relevanceLanguage: 'en',
-    publishedAfter: '2019-08-04T00:00:00Z',
+	url: 'https://www.googleapis.com/youtube/v3/search?',
+	KEY: 'AIzaSyAiFBt5vI45s1m5HKFrzpGyjcSDrnFpvhE',
+	part: 'snippet',
+	type: 'video',
+	maxResults: '15',
+	order: 'date',
+	videoCaption: 'closedCaption',
+	relevanceLanguage: 'en',
+	publishedAfter: '2019-08-04T00:00:00Z',
   },
-  load() {
-    const { url, KEY, part, type, maxResults, order, searchFor, videoCaption, relevanceLanguage, publishedAfter } = this.params; 
-    fetch(`${url}part=${part}&maxResults=${maxResults}&order=${order}&publishedAfter=${publishedAfter}&q=${searchFor}&relevanceLanguage=${relevanceLanguage}&type=${type}videoCaption=${videoCaption}&key=${KEY}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then(data => {
-      return data.json();
-    })
-    .then(result => {
-      this.createHTML(result);
-    })
-    .catch((error) => alert(`There has been a problem with your fetch operation: ${error.message}`));    
+
+  getData(searchFor) {
+	const { url, KEY, part, type, maxResults, order, videoCaption, relevanceLanguage, publishedAfter } = this.params;
+	let $this = this;
+
+	fetch(`${url}part=${part}&maxResults=${maxResults}&order=${order}&publishedAfter=${publishedAfter}&q=${searchFor}&relevanceLanguage=${relevanceLanguage}&type=${type}videoCaption=${videoCaption}&key=${KEY}`, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+	.then(data => {
+		return data.json();
+	})
+	.then(result => {
+		if (result && result.error) {
+			this.showMessage(result.error.message);
+		} else {
+			$form.closest(".form-wrapper").classList.remove("full");
+			this.createHTML(result);
+		}
+	})
+	.catch((error) => $this.showMessage(error));
   },
+
   createHTML(data) {
-    if(data) { 
-      list.innerHTML = '';
-      document.querySelector('.message').style.display = 'none';
-    }
-    data.items.forEach( (element, number) => {
-      let title = element.snippet.title;
-      let date = element.snippet.publishedAt.slice(0 , 10);
-      let id = element.id.videoId;
-      list.innerHTML += `
-      <div class="item">
-        <div class="title">
-          <h2>${title}</h2> 
-          <p class="date">${date}</p>
-          <span class="open"></span>
-          <span class="number">${number+1}</span>
-        </div>
-        <div class="video">
-          <div class="video__inner">
-            <iframe class="frame" src="https://www.youtube.com/embed/${id}" allow="camera;microphone" allowfullscreen="allowfullscreen"></iframe>
-          </div>
-        </div>
-      </div>`;
-    });
-    const listItems = document.getElementsByClassName('title');
-    for (let i = 0; i < listItems.length; i++) {
-      listItems[i].addEventListener('click', function() {
-        let title = this;
-            title.classList.toggle('active');
-        let video = this.nextElementSibling;
-            video.classList.toggle('active');
-            video.scrollIntoView({behavior: 'smooth'});
-        let number = this.lastElementChild;
-            number.classList.toggle('active');
-        let open = this.lastElementChild.previousElementSibling;
-            open.classList.toggle('active');
-      });
-    }
+	const $list = document.querySelector('.list');
+
+    if(data) {
+		$list.innerHTML = '';
+		document.querySelector('.message').style.display = 'none';
+	}
+
+	if (data.items.length) {
+		data.items.forEach( (element, number) => {
+			let title = element.snippet.title;
+			let date = element.snippet.publishedAt.slice(0 , 10);
+			let id = element.id.videoId;
+
+			$list.innerHTML += `
+			<div class="item">
+				<div class="title">
+				<h2>${title}</h2>
+				<p class="date">${date}</p>
+				<span class="open"></span>
+				<span class="number">${number + 1}</span>
+				</div>
+				<div class="video">
+				<div class="video__inner">
+					<iframe class="frame" src="https://www.youtube.com/embed/${id}" allow="camera;microphone" allowfullscreen="allowfullscreen"></iframe>
+				</div>
+				</div>
+			</div>`;
+		});
+
+		const listItems = document.getElementsByClassName('title');
+
+		for (let i = 0; i < listItems.length; i++) {
+			listItems[i].addEventListener('click', function() {
+				let title = this;
+					title.classList.toggle('active');
+				let video = this.nextElementSibling;
+					video.classList.toggle('active');
+					video.scrollIntoView({behavior: 'smooth'});
+				let number = this.lastElementChild;
+					number.classList.toggle('active');
+				let open = this.lastElementChild.previousElementSibling;
+					open.classList.toggle('active');
+			});
+		}
+	}
+  },
+
+  showMessage(message) {
+	let $errorMesage = document.querySelector('.error-message');
+
+	$errorMesage.innerHTML = `<p>${message}</p>`;
+	$errorMesage.classList.remove("hide");
   }
 }
 
-YOUTUBE.init();
 
+// Search for Youtube video by Key words
+$form.addEventListener("submit", function(event) {
+	event.preventDefault();
 
+	let $input = document.querySelector(".form__input");
+
+	if ($input && $input.value !== "") {
+		YOUTUBE.getData($input.value)
+	}
+})
