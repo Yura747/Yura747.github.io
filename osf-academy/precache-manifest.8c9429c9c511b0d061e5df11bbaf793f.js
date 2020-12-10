1f75750e7f7bd742bb75b59eabe994e054a7e71c
Yuriy Nibit <ynibit@speroteck.com>
2020-12-10 12:53:27 +0200

    osf-academy - init

---
+++ osf-academy/precache-manifest.8c9429c9c511b0d061e5df11bbaf793f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0067556d9a737495d9151c9832000530",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d83daeba2abe6df68ae5",
+    "url": "./static/css/2.ed4a10d6.chunk.css"
+  },
+  {
+    "revision": "cdb32bfcae01fe4c2d3f",
+    "url": "./static/css/main.b9d09fc1.chunk.css"
+  },
+  {
+    "revision": "d83daeba2abe6df68ae5",
+    "url": "./static/js/2.56a163df.chunk.js"
+  },
+  {
+    "revision": "cdb32bfcae01fe4c2d3f",
+    "url": "./static/js/main.314227b2.chunk.js"
+  },
+  {
+    "revision": "496add8ef57d77596ac7",
+    "url": "./static/js/runtime~main.81cd7cb3.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
